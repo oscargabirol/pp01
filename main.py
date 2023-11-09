985165848de200ec5081df6c00843898915bd6f8
Oscar <146021471+oscargabirol@users.noreply.github.com>
2023-11-09 01:03:05 +0100

    Signal and Slots

--- main.py
+++ main.py
@@ -12,11 +12,20 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("My App")
 
         button = QPushButton("Press Me!")
+        button.setCheckable(True)
+        button.clicked.connect(self.the_button_was_clicked)
+        button.clicked.connect(self.the_button_was_toggled)
 
         self.setFixedSize(QSize(400, 300))
 
         # Set the central widget of the Window.
         self.setCentralWidget(button)
+
+    def the_button_was_clicked(self):
+        print("Clicked!")
+
+    def the_button_was_toggled(self, checked):
+        print("Checked?", checked)
 
 
 app = QApplication(sys.argv)
